d7342dbed4ce0c72f5ee6e1ffc558165b89f650c
Nikhil Arora <nikhilarora160406@gmail.com>
2026-03-03 16:20:39 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,59 +1,44 @@
-import java.util.Stack;
-
-class PalindromeChecker {
-
-    // Private constructor not needed since all methods are instance methods
-    // Internal data structures are hidden
-    private Stack<Character> stack;
-
-    public PalindromeChecker() {
-        stack = new Stack<>();
-    }
-
-    // Public method to check palindrome
-    public boolean checkPalindrome(String word) {
-
-        // Clear stack if reused
-        stack.clear();
-
-        // Push all characters into the stack
-        for (char c : word.toCharArray()) {
-            stack.push(c);
-        }
-
-        // Compare characters by popping
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != stack.pop()) {
-                return false; // Not a palindrome
-            }
-        }
-
-        return true; // Palindrome
-    }
-}
-
-// Main application class
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+        String input = "level";
 
-        PalindromeChecker checker = new PalindromeChecker();
 
-        String word1 = "radar";
-        String word2 = "hello";
+        PalindromeStrategy strategy = new StackStrategy();
 
-        System.out.println("Checking: " + word1);
-        if (checker.checkPalindrome(word1)) {
-            System.out.println(word1 + " is a Palindrome.");
-        } else {
-            System.out.println(word1 + " is NOT a Palindrome.");
+
+        boolean isPalindrome = strategy.check(input);
+
+        // Output results
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
+
+
+interface PalindromeStrategy {
+    boolean check(String input);
+}
+
+
+class StackStrategy implements PalindromeStrategy {
+
+    @Override
+    public boolean check(String input) {
+
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+
+
+        for (char c : input.toCharArray()) {
+            stack.push(c);
         }
 
-        System.out.println("\nChecking: " + word2);
-        if (checker.checkPalindrome(word2)) {
-            System.out.println(word2 + " is a Palindrome.");
-        } else {
-            System.out.println(word2 + " is NOT a Palindrome.");
+
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
         }
+        return true;
     }
 }
