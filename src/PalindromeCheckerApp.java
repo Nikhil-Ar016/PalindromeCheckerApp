457af7581d2cdecc0996bce7e2e333f0c838a7a0
Nikhil-Ar016 <nikhilarora160406@gmail.com>
2026-03-03 15:18:33 +0530

    UC6

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,27 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String word = "madam";
-        Stack<Character> stack = new Stack<>();
 
-        // Push each character into the stack
-        for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Push and enqueue characters
+        for (char c : word.toCharArray()) {
+            stack.push(c);      // LIFO
+            queue.offer(c);     // FIFO
         }
 
         boolean isPalindrome = true;
 
-        // Pop characters and compare with original string
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != stack.pop()) {
+        // Compare pop (stack) vs dequeue (queue)
+        while (!stack.isEmpty()) {
+            if (!stack.pop().equals(queue.poll())) {
                 isPalindrome = false;
                 break;
             }
