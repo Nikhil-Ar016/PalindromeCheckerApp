817a2b45ab6273a94f528bd7436a52022d2cc5e4
Nikhil Arora <nikhilarora160406@gmail.com>
2026-03-03 16:03:43 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,59 +1,39 @@
-import java.util.Stack;
-
-class PalindromeChecker {
-
-    // Private constructor not needed since all methods are instance methods
-    // Internal data structures are hidden
-    private Stack<Character> stack;
-
-    public PalindromeChecker() {
-        stack = new Stack<>();
-    }
-
-    // Public method to check palindrome
-    public boolean checkPalindrome(String word) {
-
-        // Clear stack if reused
-        stack.clear();
-
-        // Push all characters into the stack
-        for (char c : word.toCharArray()) {
-            stack.push(c);
-        }
-
-        // Compare characters by popping
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) != stack.pop()) {
-                return false; // Not a palindrome
-            }
-        }
-
-        return true; // Palindrome
-    }
-}
-
-// Main application class
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+        // Input as per the requirement example
+        String input = "racecar";
 
-        PalindromeChecker checker = new PalindromeChecker();
+        // Create an instance of the service (Encapsulation)
+        PalindromeService service = new PalindromeService();
 
-        String word1 = "radar";
-        String word2 = "hello";
+        // Call the service method
+        boolean isPalindrome = service.checkPalindrome(input);
 
-        System.out.println("Checking: " + word1);
-        if (checker.checkPalindrome(word1)) {
-            System.out.println(word1 + " is a Palindrome.");
-        } else {
-            System.out.println(word1 + " is NOT a Palindrome.");
-        }
-
-        System.out.println("\nChecking: " + word2);
-        if (checker.checkPalindrome(word2)) {
-            System.out.println(word2 + " is a Palindrome.");
-        } else {
-            System.out.println(word2 + " is NOT a Palindrome.");
-        }
+        // Output results
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
+}
+
+
+class PalindromeService {
+
+   
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        // Compare characters moving inward
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false; // Not a palindrome
+            }
+            start++;
+            end--;
+        }
+        return true; // Is a palindrome
+    }
+}
 }
