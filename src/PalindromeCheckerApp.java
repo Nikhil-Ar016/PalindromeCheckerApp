1229fa54af548f68bc419a329c9f46b8086db8ef
Nikhil-Ar016 <nikhilarora160406@gmail.com>
2026-03-03 14:56:57 +0530

    UC4

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,31 @@
-import java.util.Stack;
-import java.util.Queue;
-import java.util.LinkedList;
-
 public class PalindromeCheckerApp {
-
-    public static boolean checkUsingSimpleReverse(String word) {
-        String reversed = "";
-        for (int i = word.length() - 1; i >= 0; i--) {
-            reversed += word.charAt(i);
-        }
-        return word.equals(reversed);
-    }
-
 
     public static void main(String[] args) {
 
-        System.out.println(checkUsingSimpleReverse("abba"));
+        String word = "level";
+
+        // Convert String to char array
+        char[] characters = word.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
